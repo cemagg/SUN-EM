c4a1ac8c8181d17b0cd0d3d39dc94a8026647787
19171072 <53252475+19171072@users.noreply.github.com>
2019-08-12 08:13:41 +0200

    CBFM solver + MBF generator

---
+++ reduceMBFJacques.m
@@ -0,0 +1,14 @@
+function [reducedMBF] = reduceMBFJacques(Const, fullMBF)
+%REDUCEMBFJACQUES Summary of this function goes here
+%   Detailed explanation goes here
+[U,S,V] = svd(fullMBF, 0);
+thr = Const.MBFthreshold;
+
+if size(S,1)>1
+    numBases = sum(diag(S)./max(diag(S))>(1/thr),1);
+    reducedMBF = U(:, 1:numBases);
+else
+    reducedMBF = U(:, 1:1);
+end
+end
+
---
+++ runJacquesCBFM.m
@@ -0,0 +1,75 @@
+function [cbfm] = runJacquesCBFM(Const, Solver_setup, zMatrices, yVectors, xVectors, mbfs)
+%RUNJACQUESCBFM Summary of this function goes here
+%   Detailed explanation goes here
+    cbfm = [];
+    Nmom = Solver_setup.num_mom_basis_functions;
+    numArrEls = Solver_setup.num_finite_array_elements;
+    cbfm.numSols = 1;
+    cbfm.Isol = complex(zeros(Nmom, 1));
+    
+    numR = mbfs.totRedMBFs;
+    numC = mbfs.totRedMBFs;
+    
+    cbfm.Zred = complex(zeros(numR, numC));
+    cbfm.Vred = complex(zeros(numR, 1));
+    
+    for p=1:numArrEls
+        PindxEnd = 0;
+        PindxStart = 0;
+        numMBFsP = 0;
+        domain_p_bfs = Solver_setup.rwg_basis_functions_domains{p};
+        N_p = length(domain_p_bfs);
+        
+        domainPCbfs = complex(zeros(N_p, mbfs.numRedMBFs(p, 1)));
+        for(red_ii=1:mbfs.numRedMBFs(p, 1))
+            domainPCbfs(:, red_ii) = mbfs.RedIsol(domain_p_bfs, red_ii, p, 1);
+        end
+        Vrwg = yVectors.values(domain_p_bfs, 1);
+        for q = 1:numArrEls
+            domain_q_bfs = Solver_setup.rwg_basis_functions_domains{q};
+            N_q = length(domain_q_bfs);
+            domainQCbfs = complex(zeros(N_q, mbfs.numRedMBFs(q, 1)));
+            for red_ii=1:mbfs.numRedMBFs(q, 1)
+                domainQCbfs(:, red_ii) = mbfs.RedIsol(domain_q_bfs, red_ii, q,1);
+            end
+            [Z] = zMatrices.values(domain_p_bfs, domain_q_bfs, 1);
+            
+            numMBFsP = 0;
+            for domain = 1:(p-1)
+                 numMBFsP = numMBFsP + mbfs.numRedMBFs(domain,1);
+            end
+            PindxStart = numMBFsP+1;
+            PindxEnd   = (PindxStart - 1) + mbfs.numRedMBFs(p,1);
+                 
+            numMBFsQ = 0;
+            for domain = 1:(q-1)
+                 numMBFsQ = numMBFsQ + mbfs.numRedMBFs(domain,1);
+            end
+            QindxStart = numMBFsQ+1;
+            QindxEnd   = (QindxStart - 1) + mbfs.numRedMBFs(q,1);
+            cbfm.Zred(PindxStart:PindxEnd, QindxStart:QindxEnd) = (domainPCbfs)'*Z*domainQCbfs;
+        end
+        
+        cbfm.Vred(PindxStart:PindxEnd) = (domainPCbfs)' * Vrwg;
+    end
+   [L,U] = lu(cbfm.Zred);
+   b = L\cbfm.Vred;
+   cbfm.Ired = U\b;
+   for p=1:numArrEls
+       domain_p_bfs = Solver_setup.rwg_basis_functions_domains{p};
+       if (p > 1)
+           offset = offset + mbfs.numRedMBFs(p-1, 1);
+       else
+           offset = 0;
+       end
+       
+       for red_ii=1:mbfs.numRedMBFs(p, 1)
+           fak = cbfm.Ired(offset + red_ii);
+           cbfm.Isol(domain_p_bfs, 1) = cbfm.Isol(domain_p_bfs, 1) + fak.*mbfs.RedIsol(domain_p_bfs,red_ii,p,1);
+       end
+   end
+   cbfm.relError = calculateErrorNormPercentage(xVectors.Isol(:,1), cbfm.Isol(:,1));
+   message_fc(Const,sprintf('Rel. error norm. for Sol. %d of %d compared to FEKO sol. %f percent.',1, 1, cbfm.relError));
+   
+end
+
