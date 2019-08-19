0ca662b93d6154ae4a4b67109bc8facf4bdc79dc
19171072 <53252475+19171072@users.noreply.github.com>
2019-08-19 13:10:19 +0200

    Add files via upload

--- src/MBFinterface/CMAMBFinterface/reduceCMAMBFs.m
+++ src/MBFinterface/CMAMBFinterface/reduceCMAMBFs.m
@@ -2,7 +2,7 @@ function [reducedMBF] = reduceMBFJacques(Const, fullMBF)
 %REDUCEMBFJACQUES Summary of this function goes here
 %   Detailed explanation goes here
 [U,S,V] = svd(fullMBF, 0);
-thr = Const.MBFthreshold;
+thr = 10000000;
 
 if size(S,1)>1
     numBases = sum(diag(S)./max(diag(S))>(1/thr),1);
