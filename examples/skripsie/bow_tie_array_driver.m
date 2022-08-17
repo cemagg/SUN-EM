9ce30944f05db0b5597ae51d348bfee993b2f848
21819203 <21819203@sun.ac.za>
2022-08-17 11:44:53 +0200

    Zmn phase and magnitude plots

--- examples/skripsie/bow_tie_array_driver.m
+++ examples/skripsie/bow_tie_array_driver.m
@@ -66,3 +66,48 @@ Const.SUNEMdgfmstrfilename     =  '';%'sunem_dgfm_bow_tie_array.str';
 [Solution] = runEMsolvers(Const, Solver_setup, zMatrices, yVectors, xVectors);
 
 %Solution.mom has all the solver settings
+
+%Top plot
+ax1 = nexttile;
+yvalues = log10(abs(zMatrices.values(1,1,1:5))); % build 3D array of all of individuals to manipulate as one
+yvalues=reshape(permute(yvalues,[5,4,3,2,1]),5,[]);  % rearrange by plane first, row & column and put in columns
+plot(1:5,yvalues)                                    % plot each column against the y vector
+
+yvalues = log10(abs(zMatrices.values(1,10,1:5))); 
+yvalues=reshape(permute(yvalues,[5,4,3,2,1]),5,[]);
+hold on;
+plot(1:5,yvalues);
+
+ 
+yvalues = log10(abs(zMatrices.values(1,20,1:5))); 
+yvalues=reshape(permute(yvalues,[5,4,3,2,1]),5,[]);
+hold on;
+plot(1:5,yvalues);
+
+legend('m,n = 1,1','m,n = 1,10','m,n = 1,20');
+title(ax1,'magnitude plots');
+hold off
+
+%bottom plot
+ax2 = nexttile;
+angle = phase(zMatrices.values(1,1,1:5));
+angle=reshape(permute(angle,[5,4,3,2,1]),5,[]);
+hold on;
+plot(1:5,angle);
+
+angle = phase(zMatrices.values(1,10,1:5));
+angle=reshape(permute(angle,[5,4,3,2,1]),5,[]);
+hold on;
+plot(1:5,angle);
+
+angle = phase(zMatrices.values(1,20,1:5));
+angle=reshape(permute(angle,[5,4,3,2,1]),5,[]);
+hold on;
+plot(1:5,angle);
+
+legend('m,n = 1,1','m,n = 1,10','m,n = 1,20');
+title(ax2,'Phase plots');
+hold off
+
+%Link the axes
+linkaxes([ax1,ax2],'x');
