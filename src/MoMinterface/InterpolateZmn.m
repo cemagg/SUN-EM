b62001cfe426a2c4749c3e59da387faf934f90be
STB\21819203 <108519922+21819203@users.noreply.github.com>
2022-10-18 15:28:27 +0200

    Interpolate 50%

--- src/MoMinterface/InterpolateZmn.m
+++ src/MoMinterface/InterpolateZmn.m
@@ -20,8 +20,25 @@ function [Interpolate_Zmn] = InterpolateZmn(Const, Solver_setup, zMatricesINTERP
 
 
     narginchk(3,3);
+   
+     % Initialise the return values
+     zMatricesINTERPp = [];
+      zMatricesINTERPp.name = zMatricesINTERPp;
+%      numSols = 1;
+%      Interpolate_Zmn.numSols = numSols;
+     
+      FrequencySamples = [];
+      zMatricesINTERPreal = zeros(510, 50, 510);  
+      zMatricesINTERPimaginary = zeros(510, 50, 510);
+      %interpolatedreal = zeros(510, 100);
+      %interpolatedImaginary = zeros(510, 100);
+      interpolatedreal = [];
+      interpolatedImaginary = [];
+     Interpolate_Zmn = [];
 
-     %zMatricesINTERP;
+
+
+
       frequency = Solver_setup.frequencies.samples;
       freqStart = frequency(1);
       freqEnd = frequency(100);
@@ -30,80 +47,50 @@ function [Interpolate_Zmn] = InterpolateZmn(Const, Solver_setup, zMatricesINTERP
       fstep = 2;
       RWGmBasis = Const.numMoMbasis;
       RWGnBasis = Const.numMoMbasis;
-      FrequencySamples = [];
-      zMatricesINTERPreal = zeros(510, 50, 510); 
-      zMatricesINTERPimaginary = zeros(510, 50, 510);
-      inerpolatedreal = [];
-      interpolatedImaginary = [];
-      Interpolate_Zmn = [];
-      interpolatedreal = [];
-      interpolatedImaginary = []; 
             
 %  
 
-
-%  for freq = 2:fstep:numFreq
+%Extract the real and imaginary values preparing for interpolation,
   FrequencySamples = [FrequencySamples; frequency(2:fstep:numFreq)];
    for m = 1:RWGmBasis 
     for n = 1:RWGnBasis
          col = 0;
       if m ~= n
         for freq = 2:fstep:numFreq
+            %tic
             col = col+1;
-         %FrequencySamples = [FrequencySamples; frequency(freq)];
-         %FrequencysampleUnique(n) = Frequencysamples + linspace(0, 1, length(FrequencySamples))*10E-3
-         zMatriceCalculatedReal = real(zMatricesINTERP(m,n,freq));
+         zMatriceCalculatedReal = real(zMatricesINTERP(m,n,freq)); 
          zMatricesINTERPreal(n,col,m) = zMatriceCalculatedReal;
          zMatriceCalculatedImaginary = imag(zMatricesINTERP(m,n,freq));
          zMatricesINTERPimaginary(n,col,m) = zMatriceCalculatedImaginary;
-
+       %  zMatricesINTERPp.setupTime(freq) = toc;
        end 
-     end
+      end
+      
    % row = row+1;
    end
-  end
+   end
 
-for m = 1:RWGmBasis
+     %Apply interpolation
+ for m = 1:RWGmBasis
     for n = 1:RWGnBasis
+       % tic
+       % tic
         if m ~= n 
-      % FrequencySamples = [FrequencySamples; frequency(2:fstep:numFreq)];
-      %Apply interpolation
       fq = (freqStart:stepSize:freqEnd);
       a = zMatricesINTERPreal(n,:,m);
-      xq = interp1(FrequencySamples,a,fq,"linear");
+      xq = interp1(FrequencySamples,a,fq,"spline");
       interpolatedreal = [interpolatedreal; xq];
-      %xq = [];
       b = zMatricesINTERPimaginary(n,:,m);
-      yq = interp1(FrequencySamples,b,fq,"linear");
+      yq = interp1(FrequencySamples,b,fq,"spline");
       interpolatedImaginary = [interpolatedImaginary; yq];
-      %yq = [];
+      %zMatricesINTERPp.intepolationTime(n) = toc;
         end
     end
 end
- m = 1;
 
+     %Interpolatd elements (510x510)-(Diagonals)= 259590 X 99 
+     Interpolate_Zmn.values = interpolatedreal(1:fstep:numFreq) + 1i*(interpolatedImaginary(1:fstep:numFreq));
 
-
-
-% 
-% 
-%    for f = 1:99
-%          %store the elements in Zmn (2D) in a struct
-%          Zmn = reshape((zInterpolatedsamples(:,f)),[RWGmBasis,RWGnBasis]);  %interpolated data
-%          Zmnlist = [Zmnlist; Zmn];
-%    end
-% 
-% 
-% %storing the data in 3-D (2x2 matrix)xfrequency
-% row = 1;    
-% for i = 1:99   %1:numFreq
-%     for j = 1:RWGmBasis   
-%         for k = 1:RWGnBasis
-%             InterpolatedValues(j,k,i) = Zmnlist(row,k);
-% 
-%         end 
-%         row = row+1;
-%     end
-% end
 
 
